7ad32c2070a32511ecbc49742bd28fc93edae166
Eric_Lian <ExerciseBook@users.noreply.github.com>
2018-09-17 20:50:40 +0800

    Deleted a line

--- 1/3.py
+++ 1/3.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 import qrcode
 s="Let the bass kick O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A- JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA".encode("utf-8")
 qr=qrcode.QRCode()

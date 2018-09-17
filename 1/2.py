402221ff9e3da99499a5b088b2d2108e64a37ac1
Eric_Lian <ExerciseBook@users.noreply.github.com>
2018-09-17 09:22:32 +0800

    Series 1

---
+++ 1/2.py
@@ -0,0 +1,4 @@
+import base64
+
+a = "Let the bass kick O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A- JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA".encode("utf-8")
+print(str(base64.b64encode(a),"utf-8"))

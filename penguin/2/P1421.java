e3a321d20d1b9d1e72a27e6dc79b2570ce576077
Eric_Lian <ExerciseBook@users.noreply.github.com>
2018-10-16 16:30:29 +0800

    Penguin | Week 2 | Fix

--- penguin/2/P1421.java
+++ penguin/2/P1421.java
@@ -4,13 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) throws Exception {
         Scanner cin = new Scanner(System.in);
-        int ah = cin.nextInt(), an = cin.nextInt(), bh = cin.nextInt(), bn = cin.nextInt();
-		int dn = (bh * 60 + bn) - (ah * 60 + an);
-		if (dn==0) {
-			dn = 24*60;
-		}
-		int dh = dn / 60;
-		dn = dn % 60;
-        System.out.printf("%d %d\n",dh,dn);
+        int a = cin.nextInt(), b = cin.nextInt();
+        System.out.println((a * 10 + b)/19);
     }
 }
